9fe104ed7e3a8f51b98caf80d94a3c64c39b8b2a
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-07-22 11:26:03 +0200

    ReadonlyArray

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,10 +1,12 @@
+import { Negociacoes } from './../models/negociacoes.js';
 import { Negociacao } from './../models/negociacao.js';
 
 export class NegociacaoController {
     private inputData:HTMLInputElement;
     private inputquantidade:HTMLInputElement;
     private inputValor:HTMLInputElement;
-    
+    private negociacoes = new Negociacoes();
+
     constructor(){
         this.inputData = document.querySelector("#data");
         this.inputquantidade = document.querySelector("#quantidade");
@@ -13,7 +15,8 @@ export class NegociacaoController {
 
     adiciona(): void{
        const negociacao = this.criaNegociacao();
-       console.log(negociacao);
+       this.negociacoes.adiciona(negociacao);
+       console.log(this.negociacoes.lista());
        this.limpaFormulario();
     }
 
