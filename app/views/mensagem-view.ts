26ada977946be7c278be23abeae8261a2ee0e339
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-07-23 12:36:04 +0200

    Heranca

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,11 +1,7 @@
-export class MensagemView {
+import { View } from './view.js';
+export class MensagemView extends View {
 
-    private element:HTMLElement;
-
-    constructor(seletor:string) {
-        this.element = document.querySelector(seletor);
-    }
-
+    
     template(model:string):string{
         return `
             <p class="alert alert-info">${model}</p>
@@ -14,7 +10,7 @@ export class MensagemView {
 
     update(model:string):void{
         const template = this.template(model);
-        this.element.innerHTML = template;
+        this.elemento.innerHTML = template;
 
     }
 }
