5ab4f165e085bbdae8afd18adf2503f1112255b1
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-07-23 12:09:55 +0200

    Mensagem view

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,10 +1,12 @@
 import { NegociacoesView } from './../views/negociacoes-view.js';
 import { Negociacoes } from './../models/negociacoes.js';
 import { Negociacao } from './../models/negociacao.js';
+import { MensagemView } from '../views/mensagem-view.js';
 export class NegociacaoController {
     constructor() {
         this.negociacoes = new Negociacoes();
         this.negociacoesView = new NegociacoesView('#negociacoesView');
+        this.mensagemView = new MensagemView('#mensagemView');
         this.inputData = document.querySelector("#data");
         this.inputquantidade = document.querySelector("#quantidade");
         this.inputValor = document.querySelector("#valor");
@@ -14,6 +16,7 @@ export class NegociacaoController {
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Necociacao adiconada com sucesso');
         this.limpaFormulario();
     }
     criaNegociacao() {
